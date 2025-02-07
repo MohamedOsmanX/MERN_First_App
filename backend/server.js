0beae3af308367329bb5a9d26594f503df61d914
BomBaMo <hamarsaysaab1@gmail.com>
2025-02-07 22:45:13 +0400

    Completed DB

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,7 @@
 import express from 'express';
 import dotenv from "dotenv";
 import { connectDB } from './config/db.js';
-import Product from './models/product.model.js'; 
+import productRoutes from "./routes/product.route.js"
 
 dotenv.config();
 
@@ -9,23 +9,9 @@ const app = express();
 
 app.use(express.json()); //allows us to accept json data in the req.body
 
-app.post("/api/products", async (req,res) => {
-    const product = req.body; //user will send this data
+app.use("/api/products", productRoutes);
 
-    if(!product.name || !product.price || !product.image) {
-        return res.status(400).json({success:false, message: "Please provide all fields "});
-    }
-
-    const newProduct = new Product(product);
-    
-    try {
-        await newProduct.save();
-        res.status(201).json({success: true, data: newProduct});
-    } catch (error) {
-        console.error("Error in Create product:", error.message);
-        res.status(500).json({success: false, message: "Server Error"});
-    }
-});
+const PORT = process.env.PORT;
 
 app.listen(5000, () => {
     connectDB();
